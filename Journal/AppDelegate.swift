f7d7649be4a525fc357df3d70589a0e17eb62a4b
Mary Salemme <mary.salemme@bglgroup.co.uk>
2019-11-21 16:11:22 +0000

    Add visit tracking and geoCoder

--- Journal/AppDelegate.swift
+++ Journal/AppDelegate.swift
@@ -5,14 +5,45 @@ import UserNotifications
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
-  
-  let center = UNUserNotificationCenter.current()
+  // Needed for reverse geocoding. It takes some coordinates and returns an address
+  static let geoCoder = CLGeocoder()
+  let notificationCenter = UNUserNotificationCenter.current()
   let locationManager = CLLocationManager()
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     let newColor = UIColor(red: 110/255, green: 47/255, blue: 156/255, alpha: 1)
     UITabBar.appearance().tintColor = newColor
+    
+    notificationCenter.requestAuthorization(options: [.alert, .sound]) { granted, error in
+      
+    }
+    
+    locationManager.requestAlwaysAuthorization()
+    locationManager.startMonitoringVisits()
+    locationManager.delegate = self as CLLocationManagerDelegate
+    
     return true
   }
 }
 
+extension AppDelegate: CLLocationManagerDelegate {
+  /// Callback from `CLLOcationManager` when the new visit is recorded
+  func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
+    // create location from the coordinates of CLVisit
+    let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
+    
+    AppDelegate.geoCoder.reverseGeocodeLocation(clLocation) { (placemarks, error) in
+      if let place = placemarks?.first {
+        let description = "\(place)"
+        self.newVisitReceived(visit, description: description)
+      } else if let error = error {
+        assertionFailure(error as! String)
+      }
+    }
+  }
+  
+  /// Saves the location to disk
+  func newVisitReceived(_ visit: CLVisit, description: String) {
+    let location = Location(visit: visit, descriptionString: description)
+  }
+}
