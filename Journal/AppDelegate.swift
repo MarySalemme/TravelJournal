c80fde776fa8e80f94cf7e5c5b8c803cdaa4e429
Mary Salemme <mary.salemme@bglgroup.co.uk>
2019-11-21 17:00:37 +0000

    Add location mock

--- Journal/AppDelegate.swift
+++ Journal/AppDelegate.swift
@@ -21,7 +21,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     locationManager.requestAlwaysAuthorization()
     locationManager.startMonitoringVisits()
     locationManager.delegate = self as CLLocationManagerDelegate
-    
+    locationManager.distanceFilter = 35
+    locationManager.allowsBackgroundLocationUpdates = true
+    locationManager.startUpdatingLocation()
     return true
   }
 }
@@ -45,5 +47,64 @@ extension AppDelegate: CLLocationManagerDelegate {
   /// Saves the location to disk
   func newVisitReceived(_ visit: CLVisit, description: String) {
     let location = Location(visit: visit, descriptionString: description)
+    
+    // Creates notification content
+    let content = UNMutableNotificationContent()
+    content.title = "New Travel Journal entry 📍"
+    content.body = location.description
+    content.sound = .default
+    
+    // Creates a second long trigger and notification request with that trigger
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+    let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
+    
+    // Schedules the notification by adding request to notification center
+    notificationCenter.add(request, withCompletionHandler: nil)
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+      guard let location = locations.first else {
+        return
+      }
+      AppDelegate.geoCoder.reverseGeocodeLocation(location) { placemarks, _ in
+      if let place = placemarks?.first {
+        let description = "Fake visit: \(place)"
+        let fakeVisit = MockVisit(
+          coordinates: location.coordinate,
+          arrivalDate: Date(),
+          departureDate: Date())
+        self.newVisitReceived(fakeVisit, description: description)
+      }
+    }
+  }
+}
+
+/// Mock for CLVisit properties
+final class MockVisit: CLVisit {
+  private let mockCoordinates: CLLocationCoordinate2D
+  private let mockArrivalDate: Date
+  private let mockDepartureDate: Date
+  
+  override var coordinate: CLLocationCoordinate2D {
+    return mockCoordinates
+  }
+  
+  override var arrivalDate: Date {
+    return mockArrivalDate
+  }
+  
+  override var departureDate: Date {
+    return mockDepartureDate
+  }
+  
+  init(coordinates: CLLocationCoordinate2D, arrivalDate: Date, departureDate: Date) {
+      mockCoordinates = coordinates
+      mockArrivalDate = arrivalDate
+      mockDepartureDate = departureDate
+    super.init()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
 }
